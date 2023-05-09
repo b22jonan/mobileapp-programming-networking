2eb82f311234cc41a0ec5fcdd3dd526ab71ef6db
Jonathan Antfolk <61019188+walsand987@users.noreply.github.com>
2023-05-09 15:04:17 +0200

    filled in main activity

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,24 +4,53 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+
+        Gson gson = new Gson();
+        Type listType = new TypeToken<ArrayList<Mountain>>(){}.getType();
+        List<Mountain> mountains = gson.fromJson(json, listType);
+
+        for (Mountain mountain : mountains) {
+            Log.d("BRAH", mountain.toString());
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.RecyclerView);
+
+        mAdapter adapter = new mAdapter(this, mountains);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter.notifyDataSetChanged();
+
     }
 
 }
